ac29118b9d0686096ab31d36bc13ed1f284049c8
niou <niou.zhao@tuya.com>
2021-12-29 22:04:27 +0800

    add Q9PalindromeNumber

---
+++ src/com/zmoyi/leetcode/editor/cn/Q9PalindromeNumber.java
@@ -0,0 +1,65 @@
+/**
+<p>给你一个整数 <code>x</code> ，如果 <code>x</code> 是一个回文整数，返回 <code>true</code> ；否则，返回 <code>false</code> 。</p>
+
+<p>回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，<code>121</code> 是回文，而 <code>123</code> 不是。</p>
+
+<p> </p>
+
+<p><strong>示例 1：</strong></p>
+
+<pre>
+<strong>输入：</strong>x = 121
+<strong>输出：</strong>true
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<strong>输入：</strong>x = -121
+<strong>输出：</strong>false
+<strong>解释：</strong>从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+</pre>
+
+<p><strong>示例 3：</strong></p>
+
+<pre>
+<strong>输入：</strong>x = 10
+<strong>输出：</strong>false
+<strong>解释：</strong>从右向左读, 为 01 。因此它不是一个回文数。
+</pre>
+
+<p><strong>示例 4：</strong></p>
+
+<pre>
+<strong>输入：</strong>x = -101
+<strong>输出：</strong>false
+</pre>
+
+<p> </p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li><code>-2<sup>31</sup> <= x <= 2<sup>31</sup> - 1</code></li>
+</ul>
+
+<p> </p>
+
+<p><strong>进阶：</strong>你能不将整数转为字符串来解决这个问题吗？</p>
+<div><div>Related Topics</div><div><li>数学</li></div></div><br><div><li>👍 1734</li><li>👎 0</li></div>
+*/
+
+package com.zmoyi.leetcode.editor.cn;
+public class Q9PalindromeNumber{
+    public static void main(String[] args) {
+        Solution solution = new Q9PalindromeNumber().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(int x) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
